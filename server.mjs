85a00879dbf6a8d124822a8ef306d1ec760a2891
Bedo <bedonassef71@gmail.com>
2023-07-21 22:09:11 +0300

    enhance main file

--- server.mjs
+++ server.mjs
@@ -4,8 +4,10 @@ import morgan from 'morgan';
 import {ApiError} from './utils/api.error.mjs';
 import {globalErrors} from './middlewares/error.middleware.mjs';
 import {DBConnection} from './config/database.connection.mjs';
-import {bookRouter} from './routes/book.route.mjs';
+import {bookRouter} from './routes/book.router.mjs';
 import {categoryRouter} from "./routes/category.router.mjs";
+import {subCategoryRouter} from "./routes/subcategory.router.mjs";
+import {authRouter} from "./routes/auth.router.mjs";
 
 
 dotenv.config({path: 'config.env'})
@@ -26,6 +28,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/categories', categoryRouter);
+app.use('/api/v1/subcategories', subCategoryRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.all('*', (req, res, next) => {
     next(new ApiError(`can't find this route: ${req.originalUrl}`, 400));
