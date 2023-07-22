8b693f6142ef3c23a1ce8ce2454bd438820bf5f3
Bedo <bedonassef71@gmail.com>
2023-07-22 15:34:37 +0300

    make private methods to make code more readable

--- services/cart.service.mjs
+++ services/cart.service.mjs
@@ -1,49 +1,36 @@
-import {Cart} from "../models/cart.model.mjs";
-import {CartItem} from "../models/cart-item.model.mjs";
+import { Cart } from "../models/cart.model.mjs";
+import { CartItem } from "../models/cart-item.model.mjs";
 
 class CartService {
-
     async findOne(user_id) {
-        return Cart.findOne({user_id: user_id})
-            .populate({path: 'items.book_id', options: {strictPopulate: false}})
+        return Cart.findOne({ user_id: user_id })
+            .populate({ path: 'items.book_id', options: { strictPopulate: false } })
             .lean()
             .exec();
     }
 
     async create(user_id) {
-        await Cart.create({user_id: user_id});
+        await Cart.create({ user_id: user_id });
     }
 
     async addToCart(user_id, book) {
-        const {book_id, quantity} = book;
-        const cart = await Cart.findOne({user_id: user_id});
+        const cart = await this.#findCartByUserId(user_id);
+        const { book_id, quantity } = book;
 
-        const existingItem = cart.items.find(item => {
-            return item.book_id.toString() === book_id;
-        });
+        const existingItem = cart.items.find(item => item.book_id.toString() === book_id);
 
         if (existingItem) {
-            if (isNaN(quantity)) {
-                existingItem.quantity += 1;
-            } else {
-                existingItem.quantity += parseInt(quantity);
-            }
+            this.#updateExistingCartItem(existingItem, quantity);
         } else {
-            if (isNaN(quantity)) {
-                book.quantity = 1;
-            } else {
-                book.quantity = parseInt(quantity);
-            }
-            cart.items.push(book);
+            this.#addNewCartItem(cart, book, quantity);
         }
 
         await cart.save();
         return cart;
-
     }
 
     async deleteFromCart(user_id, book_id) {
-        const cart = await Cart.findOne({user_id: user_id});
+        const cart = await this.#findCartByUserId(user_id);
         const itemIndex = cart.items.findIndex(item => item.book_id.toString() === book_id);
 
         if (itemIndex !== -1) {
@@ -56,16 +43,37 @@ class CartService {
     }
 
     async deleteAllBooks(user_id, id) {
-        const cart = await Cart.findOne({user_id: user_id});
+        const cart = await this.#findCartByUserId(user_id);
         if (cart._id.toString() !== id) {
             return false;
         }
-        console.log(cart)
         cart.items = [];
-        cart.save();
+        await cart.save();
         return cart;
     }
 
+    // Private methods using # symbol
+
+    #findCartByUserId(user_id) {
+        return Cart.findOne({ user_id: user_id });
+    }
+
+    #updateExistingCartItem(existingItem, quantity) {
+        if (isNaN(quantity)) {
+            existingItem.quantity += 1;
+        } else {
+            existingItem.quantity += parseInt(quantity);
+        }
+    }
+
+    #addNewCartItem(cart, book, quantity) {
+        if (isNaN(quantity)) {
+            book.quantity = 1;
+        } else {
+            book.quantity = parseInt(quantity);
+        }
+        cart.items.push(book);
+    }
 }
 
-export {CartService};
+export { CartService };
